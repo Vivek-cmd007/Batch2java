fa6f90ac03c23578ad58c0a8c19d4ff3e54242e2
vivek Singh <dkvsviveksingh1998@gmail.com>
2024-03-12 15:53:02 +0530

    my commit

---
+++ corejava/DataTypeDemo.java
@@ -0,0 +1,23 @@
+class DataTypeDemo{
+
+	public static void main(String[] args){
+	
+byte b=10;
+int i=500;
+double d=10.5;
+float f= 30.8f;
+char ch='a';
+boolean v=true;
+
+System.out.println("b ="+b);
+System.out.println("i ="+i);
+System.out.println("d ="+d);
+System.out.println("f ="+f);
+System.out.println("ch ="+ch);
+System.out.println("v ="+v);
+
+}
+}
+
+
+
